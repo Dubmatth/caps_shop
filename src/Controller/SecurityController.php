97e8f30bb9f08dfbdc6455ebcca2cf8d499d2baf
Dubois Matthieu <dubois.matthieu@live.be>
2018-12-24 13:21:04 +0100

    Modif Stripe

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -87,7 +87,7 @@ class SecurityController extends AbstractController
 
             // Set your secret key: remember to change this to your live secret key in production
             // See your keys here: https://dashboard.stripe.com/account/apikeys
-            \Stripe\Stripe::setApiKey("sk_test_nZ9Y47e2xHwD5iAlmAen1Pmz");
+            \Stripe\Stripe::setApiKey("pk_test_eNgX8DOxVTZl9f95BP18Ljuk");
             // Token is created using Checkout or Elements!
             // Get the payment token ID submitted by the form:
             $token = $request->request->get('stripeToken');
